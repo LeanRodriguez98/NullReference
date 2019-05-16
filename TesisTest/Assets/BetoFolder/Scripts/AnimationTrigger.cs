4a031c8949851dae80cc396d48fa46fe8cf47226
Beto <betocastro1819@gmail.com>
2019-05-16 02:54:38 -0300

    Subway entrance implemented

---
+++ TesisTest/Assets/BetoFolder/Scripts/AnimationTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTrigger : MonoBehaviour
+{
+	public string animationTrigger;
+	public Animator targetAnimator;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+			targetAnimator.SetTrigger(animationTrigger);
+	}
+}
---
+++ TesisTest/Assets/BetoFolder/Scripts/TrainCaller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainCaller : MonoBehaviour
+{
+	public Animator animator;
+	public float timeToCallTrain = 5;
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+			Invoke("CallTrain", timeToCallTrain);
+	}
+
+	void CallTrain()
+	{
+		animator.SetTrigger("PlayerArrived");
+	}
+}
