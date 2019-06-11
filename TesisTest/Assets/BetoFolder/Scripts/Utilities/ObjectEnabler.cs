44be3c0f7e8eaf7d94817fe0937ea139e8b28b7d
Beto <betocastro1819@gmail.com>
2019-06-11 14:37:19 -0300

    "End of demo" screen added

--- TesisTest/Assets/BetoFolder/Scripts/Utilities/ObjectEnabler.cs
+++ TesisTest/Assets/BetoFolder/Scripts/Utilities/ObjectEnabler.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class ObjectEnabler : MonoBehaviour
 {
 	public bool m_enableOnAwake;
-	public float m_enableAtTime;
+	public float m_enableAfterSeconds;
 	public List<GameObject> m_objectsToEnable;
 
 	private void Awake()
 	{
 		if (m_enableOnAwake)
-			Invoke("EnableObjects", m_enableAtTime);
+			Invoke("EnableObjects", m_enableAfterSeconds);
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.CompareTag("Player"))
-			Invoke("EnableObjects", m_enableAtTime);
+			Invoke("EnableObjects", m_enableAfterSeconds);
 	}
 
 	private void EnableObjects()
