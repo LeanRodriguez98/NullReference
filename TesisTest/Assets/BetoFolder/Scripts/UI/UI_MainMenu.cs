e7e26cdf437ab7d82f97383613832130ad55f79e
Beto <betocastro1819@gmail.com>
2019-06-11 14:55:42 -0300

    Minor bug fixes

--- TesisTest/Assets/BetoFolder/Scripts/UI/UI_MainMenu.cs
+++ TesisTest/Assets/BetoFolder/Scripts/UI/UI_MainMenu.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class UI_MainMenu : MonoBehaviour
 {
+	private void Start()
+	{
+		Cursor.visible = true;
+		Cursor.lockState = CursorLockMode.None;
+	}
+
 	public void LoadScene(string sceneName)
 	{
 		SceneManager.LoadScene(sceneName);
