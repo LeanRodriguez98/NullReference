c41463b4db806210aa2d6f4174ca3136691948a6
Beto <betocastro1819@gmail.com>
2019-06-24 00:04:51 -0300

    Bedroom stuff...

---
+++ TesisTest/Assets/BetoFolder/Scripts/AnimatedMaterial.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Substance.Game;
+
+public class AnimatedMaterial : MonoBehaviour
+{
+	[System.Serializable]
+	public struct TimeValues
+	{
+		public SubstanceGraph substanceGraph;
+		public float speed;
+		public string updateValueName;
+	}
+
+	public TimeValues[] substanceGraphsToUpdate;
+
+	public void Update()
+	{
+		for (int i = 0; i < substanceGraphsToUpdate.Length; i++)
+		{
+			substanceGraphsToUpdate[i].substanceGraph.SetInputFloat(substanceGraphsToUpdate[i].updateValueName, Time.timeSinceLevelLoad * substanceGraphsToUpdate[i].speed);
+			substanceGraphsToUpdate[i].substanceGraph.QueueForRender();
+		}
+		Substance.Game.Substance.RenderSubstancesSync();
+	}
+}
