0c558fb741ee6ae243990897911c5c8763ba2bc7
Beto <betocastro1819@gmail.com>
2019-06-24 20:10:44 -0300

    Spining chair FTW! + sliding door with touchpad

---
+++ TesisTest/Assets/BetoFolder/Scripts/Interactables/Chair.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chair : Interactable
+{
+	public Animator animator;
+	
+	public override void Interact()
+	{
+		base.Interact();
+		animator.SetTrigger("Spin");
+	}
+}
---
+++ TesisTest/Assets/BetoFolder/Scripts/Interactables/TouchPad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchPad : Interactable
+{
+	public Animator animator;
+
+	public override void Interact()
+	{
+		base.Interact();
+		animator.SetBool("OpenDoor", !animator.GetBool("OpenDoor"));
+	}
+}
