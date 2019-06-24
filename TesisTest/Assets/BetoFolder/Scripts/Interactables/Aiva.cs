e9b458b27ed2d8e1f68ad1f8581b560be2eeacef
Beto <betocastro1819@gmail.com>
2019-06-24 01:43:26 -0300

    Room lights baked

---
+++ TesisTest/Assets/BetoFolder/Scripts/Interactables/Aiva.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aiva : Interactable
+{
+	private Animator animator;
+	private bool hasRestarted;
+
+	void Start ()
+	{
+		hasRestarted = false;
+		animator = GetComponent<Animator>();
+	}
+
+	public override void Interact()
+	{
+		base.Interact();
+
+		if (!hasRestarted)
+		{
+			if (GlitchEffect.glitchEffectInstance != null)
+			{
+				GlitchEffect.glitchEffectInstance.DisplayGlitchOn();
+				animator.SetTrigger("AivaRestarted");
+				hasRestarted = true;
+			}
+		}
+	}
+}
