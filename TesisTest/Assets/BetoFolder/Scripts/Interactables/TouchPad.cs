fee3b6e3f02547bbc3a8bed93029f3ca66a9f40c
Beto <betocastro1819@gmail.com>
2019-06-27 05:49:25 -0300

    Materials tilling updated

--- TesisTest/Assets/BetoFolder/Scripts/Interactables/TouchPad.cs
+++ TesisTest/Assets/BetoFolder/Scripts/Interactables/TouchPad.cs
@@ -5,10 +5,36 @@ using UnityEngine;
 public class TouchPad : Interactable
 {
 	public Animator animator;
+	public Material disabledMaterial;
+	public Material enabledMaterial;
+
+	private MeshRenderer meshRenderer;
+	private bool doorIsOpen;
+
+	public override void Start()
+	{
+		base.Start();
+
+		doorIsOpen = false;
+		meshRenderer = GetComponent<MeshRenderer>();
+		SetMaterial();
+	}
 
 	public override void Interact()
 	{
 		base.Interact();
+
+		doorIsOpen = !doorIsOpen;
+		SetMaterial();
 		animator.SetBool("OpenDoor", !animator.GetBool("OpenDoor"));
 	}
+
+	public void SetMaterial()
+	{
+		if (doorIsOpen)
+			meshRenderer.material = enabledMaterial;
+		else
+			meshRenderer.material = disabledMaterial;
+	}
+
 }
