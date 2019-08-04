65769dc468384fd2ecd7b5edce85c65d0f5047d7
123 <123>
2019-08-04 05:33:06 -0300

    Subtitles working, voicelines changed to suit functionality

---
+++ TesisTest/Assets/Scripts/Utilities/VoicelineTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoicelineTrigger : MonoBehaviour 
+{
+	public string[] keys;
+	public SubtitleManager subtitleManager;
+	public float waitForSeconds;
+	public bool playOnAwake;
+
+	private void Awake()
+	{
+		if(playOnAwake)
+			Invoke("LoadQueque", waitForSeconds);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player") && subtitleManager)
+		{
+			Invoke("LoadQueque", waitForSeconds);
+			gameObject.SetActive(false);
+		}
+				
+	}
+
+	private void LoadQueque()
+	{
+		subtitleManager.LoadAudioQueque(keys);
+	}
+
+}
