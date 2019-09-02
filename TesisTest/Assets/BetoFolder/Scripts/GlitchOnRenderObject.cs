4eb73e81538e6ad8b0488d1c20ee64693ccc9f55
Beto <betocastro1819@gmail.com>
2019-09-01 21:51:37 -0300

    Glitch effect now triggers when looking at Mama_AIVA

---
+++ TesisTest/Assets/BetoFolder/Scripts/GlitchOnRenderObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlitchOnRenderObject : RaycastTriggerEvent 
+{
+	public float glitchDuration;
+
+	public override void TriggerEvent()
+	{
+		GlitchEffect.glitchEffectInstance.DisplayGlitchOn(glitchDuration);
+		Destroy(this);
+	}
+}
