6d03072eb4bc875c4e6f2357bceb4b64ad14c0fd
Leandro Sebastian Rodriguez <lean1998@hotmail.com>
2019-09-22 15:31:00 -0300

    NewScreens

---
+++ TesisTest/Assets/Scripts/SubstanceManager/AnimatedMaterialSync.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Substance.Game;
+
+public class AnimatedMaterialSync : MonoBehaviour {
+
+    [System.Serializable]
+    public struct TimeValues
+    {
+        public SubstanceGraph substanceGraph;
+        public float speed;
+        public string updateValueName;
+    }
+    public TimeValues[] substanceGraphsToUpdate;
+
+	void Update () {
+        for (int i = 0; i < substanceGraphsToUpdate.Length; i++)
+        {
+            substanceGraphsToUpdate[i].substanceGraph.SetInputFloat(substanceGraphsToUpdate[i].updateValueName, Time.timeSinceLevelLoad * substanceGraphsToUpdate[i].speed);
+            substanceGraphsToUpdate[i].substanceGraph.QueueForRender();
+        }
+        Substance.Game.Substance.RenderSubstancesSync();
+    }
+}
