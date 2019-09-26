279675679c8f01c1f4a740dd5a11d00eaa514cf3
Beto <betocastro1819@gmail.com>
2019-09-25 21:52:57 -0300

    Fixed cube launcher issue

--- TesisTest/Assets/BetoFolder/Scripts/CubeLauncher.cs
+++ TesisTest/Assets/BetoFolder/Scripts/CubeLauncher.cs
@@ -7,11 +7,20 @@ public class CubeLauncher : MonoBehaviour
 	public GameObject cubePrefab;
 	public Transform spawnPosition;
 	public float throwingForce;
-
-	void Start ()
+    public string playerTag;
+	void Shoot ()
 	{
 		GameObject spawnedCube = Instantiate(cubePrefab, spawnPosition.position, spawnPosition.rotation);
 		Rigidbody cubeRB = spawnedCube.GetComponent<Rigidbody>();
 		cubeRB.AddForce(spawnPosition.up * throwingForce);
+        Destroy(gameObject);
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(playerTag))
+        {
+            Shoot();
+        }
+    }
 }
