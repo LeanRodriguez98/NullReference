b9d7698868bc39c701cdff7b0c359fa6562125ea
Leandro Sebastian Rodriguez <lean1998@hotmail.com>
2019-10-14 12:54:19 -0300

    MaterialReplacer

---
+++ TesisTest/Assets/Scripts/SubstanceManager/Editor/Editor_ReplaceMaterials.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ReplaceMaterials))]
+public class Editor_ReplaceMaterials : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        ReplaceMaterials replaceMaterials = (ReplaceMaterials)target;
+        if (GUILayout.Button("Replace"))
+        {
+            replaceMaterials.Replace();
+        }
+        base.OnInspectorGUI();
+    }
+}
---
+++ TesisTest/Assets/Scripts/SubstanceManager/ReplaceMaterials.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaceMaterials : MonoBehaviour
+{
+    public Material originalMaterial;
+    public Material materialToReplace;
+
+    public void Replace()
+    {
+        if (originalMaterial != null && materialToReplace != null)
+        {
+            GameObject[] all = Utilities.GetAllObjectsInScene().ToArray();
+            for (int i = 0; i < all.Length; i++)
+            {
+                if (all[i].GetComponent<Renderer>() != null)
+                {
+                    if (all[i].GetComponent<Renderer>().sharedMaterial == originalMaterial)
+                    {
+                        all[i].GetComponent<Renderer>().sharedMaterial = materialToReplace;
+                    }
+                }
+            }
+        }
+    }
+}
