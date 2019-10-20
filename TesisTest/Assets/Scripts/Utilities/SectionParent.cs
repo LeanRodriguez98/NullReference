ed17f322d02b1462d7b9a829c2fc3ead18e80bb8
Leandro Sebastian Rodriguez <lean1998@hotmail.com>
2019-10-20 16:49:16 -0300

    SecationParent

---
+++ TesisTest/Assets/Scripts/Utilities/SectionParent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SectionParent : MonoBehaviour
+{
+
+#if UNITY_EDITOR
+
+    private void OnEnable()
+    {
+        if (!Application.isPlaying)
+        {
+            foreach (Transform t in GetComponentsInChildren<Transform>(true))
+            {
+                t.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (!Application.isPlaying)
+        {
+            foreach (Transform t in GetComponentsInChildren<Transform>(true))
+            {
+                t.gameObject.SetActive(false);
+            }
+        }
+    }
+#endif
+}
