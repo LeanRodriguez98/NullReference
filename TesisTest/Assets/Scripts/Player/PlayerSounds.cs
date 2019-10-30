37c908cdf88f31a3373c0e491573881305fa40a2
mercedes albinati <mechialbinati@gmail.com>
2019-10-30 15:47:09 -0300

    AbyssFallSounds

---
+++ TesisTest/Assets/Scripts/Player/PlayerSounds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+public class PlayerSounds : MonoBehaviour {
+
+    public static PlayerSounds instance;
+    private AudioSource audioSource;
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	public void PlayPlayerSound(AudioClip clip, float volume)
+    {
+        audioSource.clip = clip;
+        audioSource.volume = volume;
+        audioSource.Play();
+    }
+}
---
+++ TesisTest/Assets/Scripts/Utilities/PlayerSoundTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(BoxCollider))]
+public class PlayerSoundTrigger : MonoBehaviour
+{
+    public AudioClip clip;
+    [Range(0.0f, 1.0f)] public float volume;
+    private void Start()
+    {
+        GetComponent<BoxCollider>().isTrigger = true;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (clip != null)
+            {
+                PlayerSounds.instance.PlayPlayerSound(clip, volume);
+                Destroy(gameObject);
+            }
+            else
+            {
+                Debug.LogError("Not Asigned a Clip in " + gameObject.name, gameObject);
+            }
+        }
+    }
+}
