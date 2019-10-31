03727de9ca9d7e58c01d76ddb36c764a5a406ef8
Leandro Sebastian Rodriguez <lean1998@hotmail.com>
2019-10-31 18:26:59 -0300

    AIVA RandomVoicelines

---
+++ TesisTest/Assets/Scripts/AIVA_RandomVoices.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIVA_RandomVoices : MonoBehaviour {
+
+    public string[] keys;
+    public float timeBetweenTrys;
+    public float tryProbability;
+	void Start ()
+    {
+        Invoke("TryPlayVoiceline", timeBetweenTrys);
+	}
+
+    
+
+    public void TryPlayVoiceline()
+    {
+        if (UnityEngine.Random.Range(0.0f, tryProbability) < 100f)
+        {
+            PlayVoiceline();
+        }
+        Invoke("TryPlayVoiceline", timeBetweenTrys);
+    }
+
+    public void PlayVoiceline()
+    {
+        List<string> voice = new List<string>();
+        voice.Add(keys[UnityEngine.Random.Range(0, keys.Length)]);
+        SubtitleManager.instance.LoadAudioQueque(voice.ToArray());
+    }	
+}
