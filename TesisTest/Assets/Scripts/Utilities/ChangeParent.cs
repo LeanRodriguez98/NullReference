5ca9054aaf38ef7ac49cf1d9ff6126276eefaea1
Leandro Sebastian Rodriguez <lean1998@hotmail.com>
2019-11-26 02:34:57 -0300

    LastDoorDesapearOnBedroomBugFixed

---
+++ TesisTest/Assets/Scripts/Utilities/ChangeParent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeParent : MonoBehaviour
+{
+    public GameObject objectToChangeParent;
+    public Transform newParent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            objectToChangeParent.transform.parent = newParent;
+            Destroy(this);
+        }
+    }
+}
