31971b3793e282c592696c8ee4b8795870a84c1b
Leandro Sebastian Rodriguez <lean1998@hotmail.com>
2019-11-26 02:17:25 -0300

    ScreensWhiteNoise

--- TesisTest/Assets/Scripts/Decorations/TV.cs
+++ TesisTest/Assets/Scripts/Decorations/TV.cs
@@ -7,19 +7,22 @@ public class TV : MonoBehaviour
     [System.Serializable]
     public struct BethweenTime
     {
-        [Range(0.0f, 120.0f)] public float minTime;
-        [Range(0.0f, 120.0f)] public float maxTime;
+        [Range(1.0f, 120.0f)] public float minTime;
+        [Range(1.0f, 120.0f)] public float maxTime;
     }
     public List<Material> materials;
     public bool randomStartMaterial;
     public BethweenTime changeTime;
-    private MeshRenderer my_Renderer;
+    public GameObject noisePlane;
+    [Range(0.2f,1.0f)] public float noiseDuration;
+    private MeshRenderer meshRenderer;
 
     void Start()
     {
-        my_Renderer = GetComponent<MeshRenderer>();
+        meshRenderer = GetComponent<MeshRenderer>();
         if (changeTime.minTime > changeTime.maxTime)
             changeTime.minTime = changeTime.maxTime;
+        noisePlane.SetActive(false);
         if (randomStartMaterial)
             ChangeMaterial();
         else
@@ -30,10 +33,22 @@ public class TV : MonoBehaviour
     {
         List<Material> m = new List<Material>();
         foreach (Material mat in materials)
-            if (mat.mainTexture.name != my_Renderer.material.mainTexture.name)
+            if (mat.mainTexture.name != meshRenderer.material.mainTexture.name)
                 m.Add(mat);
-        my_Renderer.material = m[Random.Range(0, m.Count)];
-        Invoke("ChangeMaterial", Random.Range(changeTime.minTime, changeTime.maxTime));
+        meshRenderer.material = m[Random.Range(0, m.Count)];
+        float time = Random.Range(changeTime.minTime, changeTime.maxTime);
+        Invoke("ChangeMaterial", time);
+        Invoke("DisplayNoiseOn", time - (noiseDuration / 2.0f));
     }
 
+    public void DisplayNoiseOn()
+    {
+        noisePlane.SetActive(true);
+        Invoke("DisplayNoiseOff",noiseDuration);
+    }
+
+    public void DisplayNoiseOff()
+    {
+        noisePlane.SetActive(false);
+    }
 }
---
+++ TesisTest/Assets/Shaders/Effects/WhiteNoise.shader
@@ -0,0 +1,58 @@
+﻿Shader "NullReference/WhiteNoise"
+{
+	SubShader
+	{
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+
+			sampler2D _MainTex;
+			float4 _Color;
+
+			float hash(uint2 hashInput)
+			{
+				uint2 q = 1103515245U * ((hashInput.x >> 1U) ^ (hashInput.yx))*abs(sin(_Time.y));
+				uint  n = 1103515245U * ((q.x) ^ (q.y >> 3U));
+				return float(n) * (1.0 / float(0xffffffffU));
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+
+				uint2 p = (i.uv*_ScreenParams);
+
+				float f = hash(p);
+
+				col = float4(f, f, f, 1.0);
+
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
