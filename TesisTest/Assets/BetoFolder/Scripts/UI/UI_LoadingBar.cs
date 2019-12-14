d2d5e592ad1a90d75866dffe40cdae2a28e62af7
Beto <betocastro1819@gmail.com>
2019-12-14 01:50:53 -0300

    New sequence for AivaIntro implemented

--- TesisTest/Assets/BetoFolder/Scripts/UI/UI_LoadingBar.cs
+++ TesisTest/Assets/BetoFolder/Scripts/UI/UI_LoadingBar.cs
@@ -12,53 +12,32 @@ public class UI_LoadingBar : MonoBehaviour
 	#endregion
 
 	[SerializeField] Image loadingBarFill;
-	[SerializeField] float timeToStartLoading;
 	[SerializeField] float loadingAmountPerFrame;
 
 	Animator animator;
 	float currentFillAmount;
 	float desiredPercentageToReach;
 
-	float tempPercentage;
-	bool runningFadeInAnimation;
-
 	void Start () 
 	{
 		animator = GetComponent<Animator>();
-		tempPercentage = 0;
 		desiredPercentageToReach = 0;
 		currentFillAmount = 0;
-		runningFadeInAnimation = false;
 		loadingBarFill.fillAmount = currentFillAmount;
 	}
 	
-	void Update () 
+	void FixedUpdate () 
 	{
-		if (!runningFadeInAnimation) return;
-
 		if (currentFillAmount < desiredPercentageToReach)
 		{
 			currentFillAmount += loadingAmountPerFrame;
 			loadingBarFill.fillAmount = currentFillAmount * 0.01f;
 		}
-		else
-		{
-			runningFadeInAnimation = false;
-			animator.SetBool("OnDisplay", false);
-		}
 	}
 
 	public void DisplayLoadingBar(int percentageToReach)
 	{
-		animator.SetBool("OnDisplay", true);
-
-		tempPercentage = percentageToReach;
-		Invoke("SetDesiredPercentage", timeToStartLoading);
-	}
-
-	void SetDesiredPercentage()
-	{
-		desiredPercentageToReach = tempPercentage;
-		runningFadeInAnimation = true;
+		animator.SetTrigger("OnDisplay");
+		desiredPercentageToReach = percentageToReach;
 	}
 }
---
+++ TesisTest/Assets/BetoFolder/Scripts/Utilities/GlitchEffectEnabler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class GlitchEffectEnabler : MonoBehaviour 
+{
+	[SerializeField] float enableAfterSeconds;
+	[SerializeField] float glitchDuration;
+
+	void Start()
+	{
+		Invoke("EnableGlitchEffect", enableAfterSeconds);
+	}
+
+	void EnableGlitchEffect()
+	{
+		GlitchEffect.glitchEffectInstance.DisplayGlitchOn(glitchDuration);
+	}
+}
