5b4821394af15c5be39655076c23cbd2ffcf0ac7
Beto <betocastro1819@gmail.com>
2019-12-13 23:06:01 -0300

    created LoadingBar_UI class

---
+++ TesisTest/Assets/BetoFolder/Scripts/UI/UI_LoadingBar.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_LoadingBar : MonoBehaviour 
+{
+	[SerializeField] Image loadingBarFill;
+	[SerializeField] float timeToStartLoading;
+	[SerializeField] float loadingAmountPerFrame;
+
+	Animator animator;
+	float currentFillAmount;
+	float desiredPercentageToReach;
+
+	float tempPercentage;
+
+	void Start () 
+	{
+		animator = GetComponent<Animator>();
+		tempPercentage = 0;
+		desiredPercentageToReach = 0;
+		currentFillAmount = 0;
+	}
+	
+	void Update () 
+	{
+		if (currentFillAmount <= desiredPercentageToReach)
+		{
+			currentFillAmount += loadingAmountPerFrame;
+			loadingBarFill.fillAmount = currentFillAmount * 0.01f;
+		}
+		else
+		{
+			animator.SetBool("OnDisplay", false);
+		}
+	}
+
+	public void DisplayLoadingBar(int percentageToReach)
+	{
+		animator.SetBool("OnDisplay", true);
+
+		tempPercentage = percentageToReach;
+		Invoke("SetDesiredPercentage", timeToStartLoading);
+	}
+
+	void SetDesiredPercentage()
+	{
+		desiredPercentageToReach = tempPercentage;
+	}
+}
