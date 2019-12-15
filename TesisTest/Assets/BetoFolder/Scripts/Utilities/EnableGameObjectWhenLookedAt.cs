5c2fba774709fa22156da79b26141bea1c0c2b8c
Beto <betocastro1819@gmail.com>
2019-12-15 04:56:45 -0300

    Loading bar sequences implemented

---
+++ TesisTest/Assets/BetoFolder/Scripts/Utilities/EnableGameObjectWhenLookedAt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableGameObjectWhenLookedAt : RaycastTriggerEvent 
+{
+	[SerializeField] GameObject objectToEnable;
+
+	public override void TriggerEvent() 
+	{
+		objectToEnable.SetActive(true);
+	}
+}
