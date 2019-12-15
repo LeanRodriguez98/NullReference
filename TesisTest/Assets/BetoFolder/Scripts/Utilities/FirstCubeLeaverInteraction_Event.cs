2ea57eb31b750050f5e8defbe09e33ecb595ba92
Beto <betocastro1819@gmail.com>
2019-12-15 04:31:24 -0300

    Player learns CubeHitLeaver mechanic, dialogue implemented

---
+++ TesisTest/Assets/BetoFolder/Scripts/Utilities/FirstCubeLeaverInteraction_Event.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FirstCubeLeaverInteraction_Event : MonoBehaviour 
+{
+	[SerializeField] GameObject playerLearnedCubeLeaverMachanic;
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag("PickUpable"))
+			playerLearnedCubeLeaverMachanic.SetActive(true);
+	}
+}
