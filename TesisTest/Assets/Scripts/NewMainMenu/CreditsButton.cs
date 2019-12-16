063eb4d900a1aa3a3a3497ee3f4b34ef4c786ffd
Leandro Sebastian Rodriguez <lean1998@hotmail.com>
2019-12-16 01:38:24 -0300

    Credits

---
+++ TesisTest/Assets/Scripts/NewMainMenu/CreditsButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace NewMainMenu
+{
+    public class CreditsButton : MenuButton
+    {
+
+        public float delay;
+        public string creditsSceneName;
+        private bool clicked = false;
+
+
+        private void OnEnable()
+        {
+            clicked = false;
+        }
+
+        private void OnMouseEnter()
+        {
+            audioSource.Play();
+        }
+
+        void OnMouseDown()
+        {
+            if (!clicked)
+            {
+                Invoke("ChangeToCredits", delay);
+                clicked = true;
+            }
+        }
+
+        private void ChangeToCredits()
+        {
+            Utilities.LoadScene(creditsSceneName);
+        }
+    }
+}
---
+++ TesisTest/Assets/Scripts/Utilities/Credits.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    public string mainMenuSceneName;
+    public KeyCode backToMainMenuKey;
+
+    public void AE_BackToMainMenu()
+    {
+        Utilities.LoadScene(mainMenuSceneName);
+    }
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(backToMainMenuKey))
+        {
+            AE_BackToMainMenu();
+        }
+    }
+}
